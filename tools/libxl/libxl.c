7aa21d091709a23d76c24b7615a8ecc4d6b7f9b5
Yang Hongyang <yanghy@cn.fujitsu.com>
2014-07-18 17:14:22 +0800

    remus drbd: Implement remus drbd replicated disk
    Implement remus-drbd-replicated-checkpointing-disk based on
    generic remus devices framework.
    
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Wen Congyang <wency@cn.fujitsu.com>
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>

--- tools/libxl/libxl.c
+++ tools/libxl/libxl.c
@@ -826,6 +826,7 @@ int libxl_domain_remus_start(libxl_ctx *ctx, libxl_domain_remus_info *info,
         }
         rds->device_kind_flags |= LIBXL__REMUS_DEVICE_NIC;
     }
+    rds->device_kind_flags |= LIBXL__REMUS_DEVICE_DISK;
 
     rds->ao = ao;
     rds->egc = egc;
