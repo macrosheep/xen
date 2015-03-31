8b301e64a1d8c99aac41254d7b85b9181b381309
Yang Hongyang <yanghy@cn.fujitsu.com>
2015-03-31 15:23:05 +0800

    COLO proxy: implement setup/teardown of COLO proxy module
    setup/teardown of COLO proxy module.
    we use netlink to communicate with proxy module.
    
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>

--- tools/libxl/libxl_internal.h
+++ tools/libxl/libxl_internal.h
@@ -2853,6 +2853,15 @@ struct libxl__remus_state {
 _hidden int libxl__netbuffer_enabled(libxl__gc *gc);
 
 /*----- colo related state structure -----*/
+typedef struct libxl__colo_proxy_state libxl__colo_proxy_state;
+struct libxl__colo_proxy_state {
+    /* set by caller of colo_proxy_setup */
+    libxl__ao *ao;
+
+    int sock_fd;
+    int index;
+};
+
 typedef struct libxl__colo_save_state libxl__colo_save_state;
 struct libxl__colo_save_state {
     libxl__checkpoint_devices_state cds;
@@ -2872,6 +2881,9 @@ struct libxl__colo_save_state {
 
     /* private, used by qdisk block replication */
     bool qdisk_setuped;
+
+    /* private, used by colo-proxy */
+    libxl__colo_proxy_state cps;
 };
 
 /*----- Domain suspend (save) state structure -----*/
